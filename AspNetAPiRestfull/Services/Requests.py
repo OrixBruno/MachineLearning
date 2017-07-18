f5ea9f1d2437b460ba118b0ef5364ab62820cf08
Tibiotas <blade-falcon@hotmail.com>
2017-07-18 08:42:59 -0300

    Alterando request

--- AspNetAPiRestfull/Services/Requests.py
+++ AspNetAPiRestfull/Services/Requests.py
@@ -1,9 +1,12 @@
-import requests
+import requests, json
+
+data = {"usuario":"bsnascimento","senha":"brq@0120","salvarUsuario":"true"}
+data = json.dumps(data)
+headers = {'content-type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+url = 'https://portal.brq.com/Login/Log'
 
 try:
-#   r = requests.get(url, params={'s': thing})
-    r = requests.get('http://localhost:5100')
-    print(r.status_code)
-    print(r.json())
+    response = requests.post(url = url, data = data, headers = headers)
+    print(response))
 except requests.ConnectionError as e:  # This is the correct syntax
     print (e)
